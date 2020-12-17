8659db126b6c083d187efaa82841bb087781b667
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-12-17 19:37:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var player, player2, player3 , player4 ,player5,player6,player7;
+var form1;
+
+
+function setup() {
+ form1 = new Form();  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  form1.backGround();
+  form1.form();
+}
